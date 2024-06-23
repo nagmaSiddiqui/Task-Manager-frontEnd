242ff130308c135a6ff60b65c94690e445e07afd
nagmaSiddiqui <106270858+nagmaSiddiqui@users.noreply.github.com>
2024-06-23 18:39:18 +0530

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api/tasks",
+  baseURL: "http://https://task-manager-backend-pw50.onrender.com/api/tasks",
 });
 
 export default api;
